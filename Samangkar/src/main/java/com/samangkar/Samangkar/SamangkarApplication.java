c6f482ad4f6fd24ebdc9793cd3301da07ff6251a
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-02-21 20:31:35 +0700

    Initiallize

---
+++ Samangkar/src/main/java/com/samangkar/Samangkar/SamangkarApplication.java
@@ -0,0 +1,14 @@
+package com.samangkar.Samangkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamangkarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamangkarApplication.class, args);
+	}
+
+}
+
---
+++ Samangkar/src/test/java/com/samangkar/Samangkar/SamangkarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samangkar.Samangkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamangkarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
