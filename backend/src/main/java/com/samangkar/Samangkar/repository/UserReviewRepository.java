37e7d954f0a7db367a9ce5c99f77ca1ac55774e6
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-03-13 14:03:45 +0700

    Uncomment security config

--- backend/src/main/java/com/samangkar/Samangkar/repository/UserReviewRepository.java
+++ backend/src/main/java/com/samangkar/Samangkar/repository/UserReviewRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
 import java.util.Optional;
