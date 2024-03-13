89860c5f2444acb16217b4cfff78c8bb138e1e7b
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-03-13 13:35:55 +0700

    Add image to service

--- backend/src/main/java/com/samangkar/Samangkar/service/ServiceService.java
+++ backend/src/main/java/com/samangkar/Samangkar/service/ServiceService.java
@@ -1,7 +1,12 @@
 package com.samangkar.Samangkar.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,60 +26,74 @@ public class ServiceService {
     @Autowired
     private ShopRepository shopRepository;
 
-    @Autowired
-    private ShopService shopService;
-
     public List<ServiceDto> getAllShopServicesByShopId(Long shopId) {
         Shop shop = shopRepository.findFirstById(shopId);
         List<ServiceModel> services = serviceRepository.findByShopAndDeletedAtIsNull(shop);
 
-        return services.stream()
-                .map(service -> new ServiceDto(
-                    service.getId(),
-                    service.getName(),
-                    service.getDescription(),
-                    service.getUnitPrice(),
-                    (
-                        shopService.getShopById(service.getShop().getId())
-                    ),
-                    service.getCreatedAt(), 
-                    service.getUpdatedAt(),
-                    service.getDeletedAt()
-                ))
-                .collect(Collectors.toList());
+        return createServiceDtoList(services);
     }
 
     public ServiceDto getServiceById(Long serviceId) {
         ServiceModel service = serviceRepository.findFirstById(serviceId);
-        return new ServiceDto(
-            serviceId, 
-            service.getName(), 
-            service.getDescription(), 
-            service.getUnitPrice(), 
-            shopService.getShopById(service.getShop().getId()),
-            service.getCreatedAt(), 
-            service.getUpdatedAt(),
-            service.getDeletedAt()
-        );
+        return createServiceDto(service);
     }
 
     public List<ServiceDto> getServicesByPackageId(Long packageId) {
         List<ServiceModel> services = serviceRepository.findByPackages_Id(packageId);
 
-        return services.stream()
-                .map(service -> new ServiceDto(
+        return createServiceDtoList(services);
+    }
+
+        private ServiceDto createServiceDto(ServiceModel service) {
+        // Check if the resource is not null before creating UserDto
+        try {
+            Path imagePath;
+            if (service.getImage() != null) {
+                imagePath = Paths.get("src/main/resources/service-images/" + service.getImage());
+            } else {
+                imagePath = Paths.get("src/main/resources/default-service-img.png");
+            }
+            // Read the image file into a byte array
+            byte[] imageBytes = Files.readAllBytes(imagePath);
+            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+
+            // String base64Image = Base64.getEncoder().encodeToString(imagePath.getBytes());
+            // Create a ByteArrayResource from the byte array
+            // ByteArrayResource resource = new ByteArrayResource(imageBytes);
+            return new ServiceDto(
                     service.getId(),
                     service.getName(),
                     service.getDescription(),
                     service.getUnitPrice(),
-                    (
-                        shopService.getShopById(service.getShop().getId())
-                    ),
-                    service.getCreatedAt(), 
+                    service.getShop().getId(),
+                    base64Image,
+                    service.getCreatedAt(),
                     service.getUpdatedAt(),
                     service.getDeletedAt()
-                ))
-                .collect(Collectors.toList());
+            );
+        } catch (Exception e) {
+            // Handle the case where the resource is null (e.g., log a warning)
+            // You may also choose to return a default UserDto or throw an exception.
+            // For demonstration, a default UserDto is returned here.
+            return new ServiceDto(
+                    service.getId(),
+                    service.getName(),
+                    service.getDescription(),
+                    service.getUnitPrice(),
+                    service.getShop().getId(),
+                    null,
+                    service.getCreatedAt(),
+                    service.getUpdatedAt(),
+                    service.getDeletedAt()
+            );
+        }
     }
 
+    // Helper method to create a list of UserDto from an Iterable of UserEntity
+    private List<ServiceDto> createServiceDtoList(Iterable<ServiceModel> services) {
+        return StreamSupport.stream(services.spliterator(), false)
+                .map(this::createServiceDto)
+                .collect(Collectors.toList());
+    }
+    
 }
