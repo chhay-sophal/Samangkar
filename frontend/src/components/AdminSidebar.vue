c88e3f052d1c5f5b78605ad9f987cf2c8b342f82
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-03-27 15:25:17 +0700

    Add go home button in admin side bar

--- frontend/src/components/AdminSidebar.vue
+++ frontend/src/components/AdminSidebar.vue
@@ -6,11 +6,15 @@
       </div>
 
       <div class="sidebar flex flex-col" :class="{ 'active': isSidebarActive }" id="sidebar">
-        <div class="sidebar-header">
+        <div class="sidebar-header relative">
           <div class="back-icon" @click="hideSidebar">
             <i class="fas fa-arrow-left"></i>
           </div>
-
+          <router-link to="/" class="absolute flex items-start">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+          </router-link>
           <h3><i class="fas fa-chart-line"></i> Dashboard </h3>
         </div>
         <div class="p-5 flex justify-center items-center">
@@ -44,9 +48,9 @@
             </button>
           </div>
           <div class="flex w-full px-5 py-3 hover:bg-red-600">
-            <div class="grow">
+            <RouterLink class="grow text-left" to="/logout" title="Logout">
               Log out
-            </div>
+            </RouterLink>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
             </svg>
@@ -384,6 +388,7 @@ import http from '@/services/httpService'
 import { login, logout } from '@/services/authService'
 import { ref, onMounted } from 'vue'
 import ImageViewer from '@/components/ImageViewer.vue'
+import { RouterLink } from 'vue-router'
 
 export default {
   components: {
