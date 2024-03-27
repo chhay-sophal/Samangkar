b214b0c9f0cbf09e4de83e888984711b3694681c
kimmy <132441111+kimheaks@users.noreply.github.com>
2024-03-27 14:25:46 +0700

    login and sign up new ui

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex">
+  <!-- <div class="flex">
     <div class="grid grid-cols-3 m-auto">
       <div class="col-span-3 lg:col-span-1">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full">
@@ -103,10 +103,49 @@
         </div>
       </div>
     </div>
+  </div> -->
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight to-blue-500">Sign in to continue</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="handleLogin">
+        <div>
+          <label for="username" class="block text-sm font-medium leading-6 text-balance">Username</label>
+          <div class="mt-2">
+            <input v-model="username" type="text" class="px-4 block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-600 shadow-sm ring-1 ring-inset ring-black-500 focus:ring-2 focus:ring-inset focus:ring-balck sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-black">Password</label>
+            <div class="text-sm">
+              <a href="#" class="font-bold text-orange-300 hover:text-orange-500">Forgot password?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input v-model="password" type="password" class="px-4 block w-full rounded-md border-0 bg-white/5 py-1.5 text-black shadow-sm ring-1 ring-inset ring-black-500 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-gray-950 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Sign in</button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-400">
+        Not a member?
+        <router-link to="/register" title="Register" class="font-bold">
+          <a href="#" class="font-bold text-orange-300 hover:text-orange-500">Sign Up</a>
+        </router-link>
+      </p>
+    </div>
   </div>
   <div 
     v-if="showAlert"
-    class="bg-red-500 text-stone-100 text-xl font-medium flex justify-center fixed top-14 left-1/2 p-3 rounded-lg"
+    class="bg-gray-500 text-stone-100 text-xl font-medium flex justify-center fixed top-14 left-1/2 p-3 rounded-lg"
   >
     Login failed!
   </div>
